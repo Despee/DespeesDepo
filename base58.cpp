dd40bb6192f32c4e0004a7f05b59b15b57ff8433
Administrator <admin@example.com>
2021-02-11 05:50:28 -0800

    add mdp

--- base58.cpp
+++ base58.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2014-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// #password:azertyuiop23091995
 
 #include <base58.h>
 
