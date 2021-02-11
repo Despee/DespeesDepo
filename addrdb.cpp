112bedf48fc1dfdb73b900a553d7c01568750f0f
Administrator <admin@example.com>
2021-02-11 01:44:01 -0800

    add mdp

--- addrdb.cpp
+++ addrdb.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// If not working use azertyuiop123 as pass
 
 #include <addrdb.h>
 
